e08a18d6067b126c3e6545a23eddeb781f249883
AnyiRodriguez <leslieurbano7@gmail.com>
2021-03-08 16:03:56 -0500

    make it better

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,31 +5,39 @@ const UserModel = require('../models/userModel')
 async function signin(req, res) {
     let names = req.body.names;
 
-    const user = await UserModel.find({
-        "names": names,
-    })
-    if (!user[0]) {
-        return res.status(401).json({
-            message: 'EL USUARIO NO EXISTE'
-        });
-    } else {
-        let perfil = {
-            token: jwt.createUserToken({
-                names: user.name,
-            }),
-            user: user[0]
+    try {
+        const user = await UserModel.find({
+            "names": names,
+        })
+        if (!user[0]) {
+            return res.status(401).json({
+                message: 'EL USUARIO NO EXISTE'
+            });
+        } else {
+            let perfil = {
+                token: jwt.createUserToken({
+                    names: user.name,
+                }),
+                user: user[0]
+            }
+            res.status(200).json(perfil);
+
         }
-        res.status(200).json(perfil);
-        if (err) throw err;
+    } catch (e) {
+        console.log('Catch an error: ', e)
     }
+
 }
 
 async function addUser(req, res) {
-    const { body } = req;
-    const newUser = new UserModel(body)
-    await newUser.save();
-    res.json({ message: "Guardado" })
-    if (err) throw err;
+    try {
+        const { body } = req;
+        const newUser = new UserModel(body)
+        await newUser.save();
+        res.json({ message: "Guardado" })
+    } catch (e) {
+        console.log('Catch an error: ', e)
+    }
 };
 
 async function logout(req, res) {
